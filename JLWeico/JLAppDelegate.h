262b858a826aad22fc10c848f049013f09f3eba9
JackLiu0903 <JackLiu0903@gmail.com>
2014-03-21 18:08:56 +0800

    Initial Commit

---
+++ JLWeico/JLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JLAppDelegate.h
+//  JLWeico
+//
+//  Created by Jack Liu on 14-3-21.
+//  Copyright (c) 2014年 Jack Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JLWeico/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JLWeico
+//
+//  Created by Jack Liu on 14-3-21.
+//  Copyright (c) 2014年 Jack Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JLAppDelegate class]));
+    }
+}
